adecc839a0165b51c8ee49daf128c4b068f52e8a
lobus <vramirez.info@gmail.com>
2023-08-28 10:51:47 -0600

    se inicia proyecto desde cero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module'; // Your routing module
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { AuthComponent } from './auth/auth.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    AuthComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
